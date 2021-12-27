f2826fa5a47b7704a7ef5d7f141b5344b909c9c1
Saad Guessous <guessous_saad@hotmail.com>
2021-12-27 19:18:30 +0200

    Set up server

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 )
 
 type person struct {
@@ -33,4 +34,16 @@ func main() {
 	}
 
 	fmt.Println("back into a Go data structure", xp2)
+
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":8087", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+
 }
